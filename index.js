50428f35eae87997393835f26465707f72e362ff
Bifard-Ios <Bifard-Ios@mail.ru>
2023-08-22 11:46:24 +0700

    realise 1.3.0

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a * b;
 const test = (a, b) => "test";
-export { add, subtract, multiply, divide, test };
+const helloy = () => console.log("helloy may package");
+export { add, subtract, multiply, divide, test, helloy };
