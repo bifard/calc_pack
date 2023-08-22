675e67bae1cfac9ee4b2b778d8eb11c12291c1fe
Bifard-Ios <Bifard-Ios@mail.ru>
2023-08-22 13:15:52 +0700

    v1.4.0

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a * b;
 const test = (a, b) => "test";
 const helloy = () => console.log("helloy may package");
-/* v1.5.0 */
+/* v1.4.0 */
 export { add, subtract, multiply, divide, test, helloy };
