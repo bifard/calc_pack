c43847cd34ac017ecaa82122e415c289fcec3f55
Bifard-Ios <Bifard-Ios@mail.ru>
2023-08-22 11:35:37 +0700

    test

--- index.js
+++ index.js
@@ -2,10 +2,5 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a * b;
-
-export {
-  add,
-  subtract,
-  multiply,
-  divide
-};
+const test = (a, b) => "test";
+export { add, subtract, multiply, divide, test };
